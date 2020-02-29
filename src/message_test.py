6529379c4c91dd6b87d3bca12c9f3dcafa5c6ff4
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-01 10:46:09 +1100

    Added some tests

--- src/message_test.py
+++ src/message_test.py
@@ -1,9 +1,17 @@
 #Raymond: tests for message.py
 
-import sys
-sys.path.append('../src/')
-import message
+from message import send
+from message import remove
+from message import edit
 
 def test_send():
-    assert message.send(12345, 1, 'The quick brown fox jumps over the lazy dog.') == {'message_id' : 1}
+    assert send(12345, 1, 'The quick brown fox jumps over the lazy dog.') == {'message_id' : 1}
+    pass
 
+def test_remove():
+    assert remove(12345, 1) == {}
+    pass
+
+def test_edit():
+    assert edit(12345, 1, 'The quick brown fox jumpes over the lazy dog.') == {}
+    pass
