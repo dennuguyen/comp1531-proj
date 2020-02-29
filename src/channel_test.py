84e7bef4ad8393c54701d953aa185c9c42805a01
dennuguyen <dennuguyen@gmail.com>
2020-02-29 20:05:48 +1100

    Move channel test files to src folder

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,6 +1,11 @@
-import sys
-sys.path.append('../src/')
 import channel
 
-# invite
-def 
+# invite() should return an empty list on success?
+def invite_test():
+
+    assert invite('123', 12, 32) == {}
+    # assert invite('123', 'a', 'b') == 
+
+# details() returns channel details of channel_id upon valid token
+# def details_test():
+#     assert details()
--- src/test/channel_test.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('../')
-import channel
-
-# invite
-def 
