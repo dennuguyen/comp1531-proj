b5c22f7866383030f5bb37a6f6dd1522249c075e
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-01 22:13:26 +1100

    Added several helper functions

--- src/message.py
+++ src/message.py
@@ -1,12 +1,44 @@
+#Message_send
+
 def send(token, channel_id, message):
     return {
         'message_id': 1,
     }
 
+#Message_remove
+
 def remove(token, message_id):
     return {
     }
 
+#Message_edit
+
 def edit(token, message_id, message):
     return {
     }
+
+#General_functions
+
+def check_token_is_valid(token):
+    return True
+
+def check_channel_id_is_valid(channel_id):
+    return True
+
+def check_user_is_member(token, channel_id):
+    return True
+
+def check_message_is_valid(message):
+    return False
+
+def check_message_id_is_valid(message_id):
+    return True
+
+def check_if_authorised_user(token, message_id):
+    return True
+
+
+
+
+
+
