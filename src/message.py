bd0fe69a9ec26c2d72050d67fd4d490b524346a3
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-03 11:51:46 +1100

    Rid of functions not required

--- src/message.py
+++ src/message.py
@@ -17,28 +17,3 @@ def edit(token, message_id, message):
     return {
     }
 
-#General_functions
-
-def check_token_is_valid(token):
-    return True
-
-def check_channel_id_is_valid(channel_id):
-    return True
-
-def check_user_is_member(token, channel_id):
-    return True
-
-def check_message_is_valid(message):
-    return False
-
-def check_message_id_is_valid(message_id):
-    return True
-
-def check_if_authorised_user(token, message_id):
-    return True
-
-
-
-
-
-
