75a64bead1d067e6ae148ed77112fbfd7792c855
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-04 19:38:32 +1100

    Reorganize functions and file names

---
+++ src/user_profile_setemail_test.py
@@ -0,0 +1,7 @@
+#Raymond: Tests for user_profile_setemail function
+
+import pytest
+import user
+
+def test_user_profile_setemail():
+    pass
---
+++ src/user_profile_sethandle_test.py
@@ -0,0 +1,7 @@
+#Raymond: Tests for user_profile_sethandle function
+
+import pytest
+import user
+
+def test_user_profile_sethandle():
+    pass
