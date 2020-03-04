1748c18137842f14b792f623f3a7d575eb890406
dennuguyen <dennuguyen@gmail.com>
2020-03-04 17:18:44 +1100

    Add more test case and categorise test case by who the inviter is

--- src/channel_invite_test.py
+++ src/channel_invite_test.py
@@ -7,14 +7,15 @@ from auth import auth_register
 def test_environment():
     u_id1, token1 = auth_register('example@unsw.com', 'password', 'The', 'User')
     u_id2, token2 = auth_register('owner@unsw.com', 'password', 'The', 'Owner')
+    u_id3, token3 = auth_register('stranger@unsw.com', 'password', 'A', 'Stranger')
     ch_id = channels_create(token2, 'New Channel', True) # u_id2 is owner
 
-    return u_id1, token1, u_id2, token2, ch_id
+    return u_id1, token1, u_id2, token2, u_id3, token3, ch_id
 
-def test_channel_invite():
+def test_channel_invite_owner_cases():
 
     # set up environment
-    u_id1, token1, u_id2, token2, ch_id = test_environment()
+    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
 
     # The owner invites the user to new channel
     assert channel_invite(token2, ch_id, u_id1) == {}
@@ -25,13 +26,46 @@ def test_channel_invite():
     # The owner re-invites the user who is already member
     assert channel_invite(token2, ch_id, u_id1) == {}
 
+def test_channel_invite_user_cases():
+
+    # set up environment
+    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
+
     # The user invites the owner to new channel
-    with pytest.raises(AccessError):
-        channel_invite(token1, ch_id, u_id2)
+    assert channel_invite(token1, ch_id, u_id2) == {}
 
     # The user invites themself to new channel
+    assert channel_invite(token1, ch_id, u_id1) == {}
+
+    # The user invites a stranger to new channel
+    assert channel_invite(token1, ch_id, u_id3) == {}
+
+def test_channel_invite_stranger_cases():
+
+    # set up environment
+    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
+    u_id4, token4 = auth_register('strangerzzz@unsw.com', 'password', 'Two', 'Strangerzzz')
+
+    # A stranger invites the owner to new channel
     with pytest.raises(AccessError):
-        channel_invite(token1, ch_id, u_id1)
+        channel_invite(token3, ch_id, u_id2)
+
+    # A stranger invites themself to new channel
+    with pytest.raises(AccessError):
+        channel_invite(token3, ch_id, u_id3)
+
+    # A stranger invites the user to new channel
+    with pytest.raises(AccessError):
+        channel_invite(token3, ch_id, u_id1)
+
+    # A stranger invites another stranger to new channel
+    with pytest.raises(AccessError):
+        channel_invite(token3, ch_id, u_id4)
+
+def test_channel_invite_validity():
+
+    # set up environment
+    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
 
     # invalid channel id i.e. channel does not exist
     with pytest.raises(InputError):
@@ -39,4 +73,4 @@ def test_channel_invite():
 
     # invalid user id i.e. user does not exist
     with pytest.raises(InputError):
-        channel_invite(token, ch_id, u_id + 1)
+        channel_invite(token2, ch_id, u_id1 + u_id2 + u_id3)
