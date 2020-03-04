31b82a891e548b718443a6d94f1f249aff29b7a7
dennuguyen <dennuguyen@gmail.com>
2020-03-05 01:23:23 +1100

    Create stub test function and comment several cases for implementation by next writer

--- src/channel_messages_test.py
+++ src/channel_messages_test.py
@@ -1,8 +1,42 @@
+import pytest
+from channel import channel_details, channel_invite
+from error import *
+from channels import channels_create
+from auth import auth_register
+
+def test_environment():
+    u_id1, token1 = auth_register('example@unsw.com', 'password', 'The', 'User')
+    u_id2, token2 = auth_register('owner@unsw.com', 'password', 'The', 'Owner')
+    u_id3, token3 = auth_register('stranger@unsw.com', 'password', 'A', 'Stranger')
+    ch_id = channels_create(token2, 'New Channel', True) # u_id2 is owner
+
+    correct_detail = {
+        'name': 'New Channel',
+        'owner_members': [
+            {
+                'u_id': u_id2,
+                'name_first': 'The',
+                'name_last': 'Owner',
+            }
+        ],
+        'all_members': [
+            {
+                'u_id': u_id1,
+                'name_first': 'The',
+                'name_last': 'User',
+            }
+        ],
+    }
+
+    return u_id1, token1, u_id2, token2, u_id3, token3, ch_id, correct_detail
+
+
+############## COMPLETE TEST FUNCTIONS BELOW ##################
 def test_channel_messages():
 
-    # create a user and channel
-    u_id, token, ch_id = create_user_channel()
-    channel_invite(token, ch_id, u_id) # add user to channel
+    # set up environment
+    u_id1, token1, u_id2, token2, u_id3, token3, ch_id, correct_detail = test_environment()
+    channel_invite(token2, ch_id, u_id1) # owner invites u_id1
 
     # add some messages to the channel
 
