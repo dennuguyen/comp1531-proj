1ed6fb3e84001cd7db4e9dc0a1534215269bafeb
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-04 20:44:53 +1100

    basically finished

--- src/auth_logout_test.py
+++ src/auth_logout_test.py
@@ -2,6 +2,7 @@ from auth_login_test import get_new_user
 import pytest
 import auth
 
+#ensure that valid users can log out sucessfully
 def test_logout(get_new_user):
 ############## SET UP STATE ##############################
     # valid u_id and token *we don't care about token*
