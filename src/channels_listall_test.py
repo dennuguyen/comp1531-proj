6708018e05b999344ac688b13d1c68c228cd480a
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-06 15:19:49 +1100

    Fixed test environment and reorganise some tests
    -Fixed test environment so Sunny is able to implement pytest fixture mroe easily
    -Reorganise some tests to different functions

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -1,62 +1,65 @@
-import pytest, channels, error
-from channel import channel_join
-from auth import auth_register
+import pytest, channel, auth, channels, error
 
 def test_environment():
 
-    # dummy information
-    token = '123'
-    channel1 = 'Channel 1'
-    channel2 = 'Channel 2'
-    channel3 = 'Channel 3'
-    channel4 = 'Channel 4'
+# Dummy information
+    token = auth.auth_register('example@unsw.edu.au', 'qwert123', 'John', 'Doe' )['token']
 
-    # create some channels
-    ch_id1 = channels_create(token, channel1, True)
-    ch_id2 = channels_create(token, channel2, True)
-    ch_id3 = channels_create(token, channel3, True)
-    ch_id4 = channels_create(token, channel4, True)
+# Create some channels
+    ch_id1 = channels.channels_create(token, 'channel 1', True)
+    ch_id2 = channels.channels_create(token, 'channel 2', True)
+    ch_id3 = channels.channels_create(token, 'channel 3', True)
+    ch_id4 = channels.channels_create(token, 'channel 4', True)
 
-    # create a list for comparison
-    example_list = {
+    return ch_id1, ch_id2, ch_id3, ch_id4, token
+
+def test_channels_listall():
+
+    token1 = test_environment()[4]
+    ch_id1 = test_environment()[0]
+    ch_id3 = test_environment()[2]
+
+    token2 = auth.auth_register('example2@unsw.com', 'qwert246', 'Jaden', 'Smith')['token']
+
+    channel.channel_join(token2, ch_id1)
+    channel.channel_join(token2, ch_id3)
+
+        test_list = {
         'channels': [
         	{
         		'channel_id': ch_id1,
-        		'name': channel1,
+        		'name': 'channel 1',
         	},
             {
         		'channel_id': ch_id2,
-        		'name': channel2,
+        		'name': 'channel 2',
         	},
             {
         		'channel_id': ch_id3,
-        		'name': channel3,
+        		'name': 'channel 3',
         	},
             {
         		'channel_id': ch_id4,
-        		'name': channel4,
+        		'name': 'channel 4',
         	},
         ],
     }
 
-    return ch_id1, ch_id2, ch_id3, ch_id4, example_list
-
-# calling listall() should return all channels with their details
-def test_channels_listall():
-
-    ch_id1, ch_id2, ch_id3, ch_id4, example_list = test_environment()
-
-    # create a user and give user some channel memberships
-    u_id, token = auth_register('example@unsw.com', 'password', 'John', 'Doe')
-    channel_join(token, ch_id1)
-    channel_join(token, ch_id3)
-
     # valid token
-    assert channels_listall(token) == example_list
+    assert channels.channels_listall(token1) == channels.channels_listall(token2) == test_list
+
+def test_channels_invalid_token():
+
+    token = test_environment()[4]
 
     # no given token
-    assert channel_list('') == {}
+    assert channels.channels_listall('') == {}
 
     # invalid token
-    invalid_token = token + 1
-    assert channel_list(invalid_token) == {}
+    
+    invalid_token = token + 'a'
+    assert channels.channels_listall(invalid_token) == {}
+
+def test_channels_list_empty():
+    token = auth.auth_register('example@unsw.com', 'password', 'Jaden', 'Smith')['token']
+    assert channels.channels_listall(token) == {}
