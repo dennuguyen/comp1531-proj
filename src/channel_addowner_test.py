591d6aaeb858041dc082d7ab15b606b0e62ba88f
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-07 13:03:51 +1100

    Update with fixture functions

--- src/channel_addowner_test.py
+++ src/channel_addowner_test.py
@@ -2,50 +2,51 @@ import pytest
 from channel import channel_join, channel_addowner
 from error import *
 from channels import channels_create
-from auth import auth_register
 
-def test_environment():
-    u_id1, token1 = auth_register('owner@unsw.com', 'password', 'The', 'Owner')
-    u_id2, token2 = auth_register('stranger@unsw.com', 'password', 'A', 'Stranger')
+@pytest.fixture(scope="module")
+def test_environment(get_new_user_1, get_new_user_2):
+    u_id1, token1 = get_new_user_1
+    u_id2, token2 = get_new_user_2
     ch_id = channels_create(token1, 'New Channel', True)
 
     return u_id1, token1, u_id2, token2, ch_id
 
 # test case where owner promotes a member to owner
-def test_channel_addowner_1():
+def test_channel_addowner_1(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, ch_id = test_environment()
+    _, token1, u_id2, token2, ch_id = test_environment
+    ch_id = channels_create(token1, 'New Channel', True)
     channel_join(token2, ch_id)
 
     # owner adds an owner
     assert channel_addowner(token1, ch_id, u_id2) == {}
 
 # test case where owner promotes owner to owner
-def test_channel_addowner_2():
+def test_channel_addowner_2(test_environment):
     
     # set up environment
-    u_id1, token1, u_id2, token2, ch_id = test_environment()
+    u_id1, token1, _, _, ch_id = test_environment
 
     # owner promotes someone
     with pytest.raises(InputError):
         channel_addowner(token1, ch_id, u_id1)
 
 # test case where owner promotes stranger to owner
-def test_channel_addowner_3():
+def test_channel_addowner_3(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, ch_id = test_environment()
+    _, token1, u_id2, _, ch_id = test_environment
 
     # owner promotes a stranger
     with pytest.raises(AccessError):
         channel_addowner(token1, ch_id, u_id2)
 
 # test case where member promotes member to owner
-def test_channel_addowner_4():
+def test_channel_addowner_4(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, ch_id = test_environment()
+    _, _, u_id2, token2, ch_id = test_environment
     channel_join(token2, ch_id)
 
     # owner promotes a stranger
@@ -53,20 +54,20 @@ def test_channel_addowner_4():
         channel_addowner(token2, ch_id, u_id2)
 
 # test case where stranger promotes member to owner
-def test_channel_addowner_5():
+def test_channel_addowner_5(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, ch_id = test_environment()
+    u_id1, _, _, token2, ch_id = test_environment
 
     # stranger promotes someone
     with pytest.raises(AccessError):
         channel_addowner(token2, ch_id, u_id1)
 
 # validity cases
-def test_channel_addowner_6():
+def test_channel_addowner_6(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, ch_id = test_environment()
+    u_id1, token1, u_id2, _, ch_id = test_environment
 
     # invalid user id
     with pytest.raises(InputError):
