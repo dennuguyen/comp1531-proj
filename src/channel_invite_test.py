43101a1d7dc3671a5c5db4464b89c25a7c7abf93
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-07 15:01:44 +1100

    Improve code with fixture

--- src/channel_invite_test.py
+++ src/channel_invite_test.py
@@ -4,18 +4,20 @@ from error import *
 from channels import channels_create
 from auth import auth_register
 
-def test_environment():
-    u_id1, token1 = auth_register('example@unsw.com', 'password', 'The', 'User')
-    u_id2, token2 = auth_register('owner@unsw.com', 'password', 'The', 'Owner')
-    u_id3, token3 = auth_register('stranger@unsw.com', 'password', 'A', 'Stranger')
+@pytest.fixture(scope="module")
+def test_environment(get_new_user_1, get_new_user_2, get_new_user_3):
+
+    u_id1, token1 = get_new_user_3
+    u_id2, token2 = get_new_user_2
+    u_id3, token3 = get_new_user_1
     ch_id = channels_create(token2, 'New Channel', True) # u_id2 is owner
 
     return u_id1, token1, u_id2, token2, u_id3, token3, ch_id
 
-def test_channel_invite_owner_cases():
+def test_channel_invite_owner_cases(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
+    u_id1, _, u_id2, token2, _, _, ch_id = test_environment
 
     # The owner invites the user to new channel
     assert channel_invite(token2, ch_id, u_id1) == {}
@@ -26,10 +28,10 @@ def test_channel_invite_owner_cases():
     # The owner re-invites the user who is already member
     assert channel_invite(token2, ch_id, u_id1) == {}
 
-def test_channel_invite_user_cases():
+def test_channel_invite_user_cases(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
+    u_id1, token1, u_id2, _, u_id3, _, ch_id = test_environment
 
     # The user invites the owner to new channel
     assert channel_invite(token1, ch_id, u_id2) == {}
@@ -40,11 +42,11 @@ def test_channel_invite_user_cases():
     # The user invites a stranger to new channel
     assert channel_invite(token1, ch_id, u_id3) == {}
 
-def test_channel_invite_stranger_cases():
+def test_channel_invite_stranger_cases(test_environment, get_new_user_4):
 
     # set up environment
-    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
-    u_id4, token4 = auth_register('strangerzzz@unsw.com', 'password', 'Two', 'Strangerzzz')
+    u_id1, _, u_id2, _, u_id3, token3, ch_id = test_environment
+    u_id4, _ = get_new_user_4
 
     # A stranger invites the owner to new channel
     with pytest.raises(AccessError):
@@ -62,10 +64,10 @@ def test_channel_invite_stranger_cases():
     with pytest.raises(AccessError):
         channel_invite(token3, ch_id, u_id4)
 
-def test_channel_invite_validity():
+def test_channel_invite_validity(test_environment):
 
     # set up environment
-    u_id1, token1, u_id2, token2, u_id3, token3, ch_id = test_environment()
+    u_id1, _, u_id2, token2, u_id3, _, ch_id = test_environment
 
     # invalid channel id i.e. channel does not exist
     with pytest.raises(InputError):
