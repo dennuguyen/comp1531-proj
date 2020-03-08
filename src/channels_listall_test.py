e596530903a00a6aef3d92884e5c1791b80dc2c2
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-09 01:44:17 +1100

    Improve the logic and upgrade with fixture

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -1,46 +1,35 @@
 import pytest, channel, auth, channels, error
 
-def test_environment():
+def test_channels_listall(get_new_user_1, get_new_user_2, get_channel_name_1, get_channel_name_2, get_channel_name_3, get_channel_name_4):
 
-# Dummy information
-    token = auth.auth_register('example@unsw.edu.au', 'qwert123', 'John', 'Doe' )['token']
+    token1 = get_new_user_1[1]
+    ch_id1 = channels.channels_create(token1, get_channel_name_1, True)
+    ch_id2 = channels.channels_create(token1, get_channel_name_2, True)
+    ch_id3 = channels.channels_create(token1, get_channel_name_3, True)
+    ch_id4 = channels.channels_create(token1, get_channel_name_4, True)
 
-# Create some channels
-    ch_id1 = channels.channels_create(token, 'channel 1', True)
-    ch_id2 = channels.channels_create(token, 'channel 2', True)
-    ch_id3 = channels.channels_create(token, 'channel 3', True)
-    ch_id4 = channels.channels_create(token, 'channel 4', True)
-
-    return ch_id1, ch_id2, ch_id3, ch_id4, token
-
-def test_channels_listall():
-
-    token1 = test_environment()[4]
-    ch_id1 = test_environment()[0]
-    ch_id3 = test_environment()[2]
-
-    token2 = auth.auth_register('example2@unsw.com', 'qwert246', 'Jaden', 'Smith')['token']
+    token2 = get_new_user_2[1]
 
     channel.channel_join(token2, ch_id1)
     channel.channel_join(token2, ch_id3)
-
-        test_list = {
+        
+    test_list = {
         'channels': [
         	{
         		'channel_id': ch_id1,
-        		'name': 'channel 1',
+        		'name': get_channel_name_1,
         	},
             {
         		'channel_id': ch_id2,
-        		'name': 'channel 2',
+        		'name': get_channel_name_2,
         	},
             {
         		'channel_id': ch_id3,
-        		'name': 'channel 3',
+        		'name': get_channel_name_3,
         	},
             {
         		'channel_id': ch_id4,
-        		'name': 'channel 4',
+        		'name': get_channel_name_4,
         	},
         ],
     }
@@ -48,9 +37,9 @@ def test_channels_listall():
     # valid token
     assert channels.channels_listall(token1) == channels.channels_listall(token2) == test_list
 
-def test_channels_invalid_token():
+def test_channels_invalid_token(get_new_user_1):
 
-    token = test_environment()[4]
+    token = get_new_user_1[1]
 
     # no given token
     assert channels.channels_listall('') == {}
@@ -60,6 +49,7 @@ def test_channels_invalid_token():
     invalid_token = token + 'a'
     assert channels.channels_listall(invalid_token) == {}
 
-def test_channels_list_empty():
-    token = auth.auth_register('example@unsw.com', 'password', 'Jaden', 'Smith')['token']
+def test_channels_list_empty(get_new_user_1):
+
+    token = get_new_user_1[1]                        
     assert channels.channels_listall(token) == {}
