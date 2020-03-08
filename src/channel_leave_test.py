5c6ac82c49b0789944c99aecbe16c3608b7c8f97
dennuguyen <dennuguyen@gmail.com>
2020-03-09 04:18:53 +1100

    Add slackr owner specific test cases

--- src/channel_leave_test.py
+++ src/channel_leave_test.py
@@ -5,33 +5,44 @@ import channels
 
 
 # case where user leaves
-def test_channel_leave_user_leaves(get_new_user_1, get_new_user_2, get_channel_name_1):
+def test_channel_leave_user_leaves(get_new_user_1, get_new_user_2, get_new_user_3,
+                                   get_channel_name_1):
 
     # get user 1
     _, token1 = get_new_user_1
 
     # get user 2
-    _, token2 = get_new_user_2
+    u_id2, token2 = get_new_user_2
+
+    # get user 3
+    _, token3 = get_new_user_3
 
     # user 1 creates a channel
     ch_name = get_channel_name_1
     ch_id = channels.channels_create(token1, ch_name, True)['channel_id']
 
-    # user 2 joins channel
+    # user 2 joins channel and is promoted to owner permissions
     channel.channel_join(token2, ch_id)
+    channel.channel_addowner(token1, ch_id, u_id2)
 
-    # user 2 leaves
+    # user 2 joins channel
+    channel.channel_join(token3, ch_id)
+
+    # user 2 (owner) leaves
     assert channel.channel_leave(token2, ch_id) == {}
-
-    # user 2 should not be member of any channels
     assert channels.channels_list(token2)['channels'] == []
 
+    # user 3 (member) leaves
+    assert channel.channel_leave(token3, ch_id) == {}
+    assert channels.channels_list(token3)['channels'] == []
 
-# case where channel is populated by a sole owner
-def test_channel_leave_sole_owner(get_new_user_1, get_new_user_2, get_channel_name_1):
+
+# case where slackr owner tries to leave
+def test_channel_leave_slackr_owner(get_new_user_1, get_new_user_2, get_channel_name_1):
 
     # get user 1
-    _, token1 = get_new_user_1
+    u_id1, token1 = get_new_user_1
+    assert u_id1 == 1  # assert user 1 is the slackr owner
 
     # get user 2
     u_id2, token2 = get_new_user_2
@@ -40,19 +51,19 @@ def test_channel_leave_sole_owner(get_new_user_1, get_new_user_2, get_channel_na
     ch_name = get_channel_name_1
     ch_id = channels.channels_create(token1, ch_name, True)['channel_id']
 
-    # user 2 joins channel
-    channel.channel_join(token2, ch_id)
-
-    # owner leaves
+    # slackr owner leaves
     with pytest.raises(error.InputError):
         channel.channel_leave(token1, ch_id)
+
+    # user 2 joins channel
+    channel.channel_join(token2, ch_id)
 
     # give user 2 owner permissions
     channel.channel_addowner(token1, ch_id, u_id2)
 
-    # user 1 (owner) can now leave
-    assert channel.channel_leave(token1, ch_id) == {}
-    assert channels.channels_list(token1)['channels'] == []
+    # slackr owner cannot leave regardless
+    with pytest.raises(error.InputError):
+        channel.channel_leave(token1, ch_id)
 
 
 # invalid channel id
