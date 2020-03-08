a00c50bf4ede15150a6fb633dfc544309b602da0
dennuguyen <dennuguyen@gmail.com>
2020-03-08 12:01:11 +1100

    Review message_edit_test.py and add test case for message edit to be removed

--- src/message_edit_test.py
+++ src/message_edit_test.py
@@ -8,87 +8,121 @@ import channels
 import error
 import message_test_helper
 
+
+# User edits their own message
 def test_message_edit():
 
-    #Setup
-        #Register test user 1
-    token = message_test_helper.get_new_user1()[1]
+    # Register test user 1 (owner)
+    email1, password1, name_first1, name_last1 = message_test_helper.get_new_user1(
+    )
+    register_retval1 = auth.auth_register(email1, password1, name_first1,
+                                          name_last1)
+    u_id1, token1 = register_retval1['u_id'], register_retval1['token']
 
-        #Create test channel 1
-    channel_id = channels.channels_create(token, 'test_channel1', True)['channel_id']
+    # Create test channel
+    ch_id = channels.channels_create(token1, 'test_channel1',
+                                     True)['channel_id']
 
-        #Send message 1
-    msg = 'The quick brown fox jumps over the lazy dog.'
-    message_id = message.message_send(token, channel_id, msg)
+    # Send message 1
+    msg_send1 = 'The quick brown fox jumps over the lazy dog.'
+    msg_id = message.message_send(token1, ch_id, msg_send1)
 
-    #Actual test
-    newmsg = 'The quick brown dog jumps over the lazy fox.'
-    message.message_edit(token, message_id, newmsg)
+    # Actual test
+    msg_send2 = 'The quick brown dog jumps over the lazy fox.'
+    message.message_edit(token1, msg_id, msg_send2)
 
     start = 0
-    assert channel.channel_messages(token, channel_id, start)['messages'][0]['message'] == newmsg    
-    
-    #Clean up (if necessary)
+    assert channel.channel_messages(
+        token1, ch_id, start)['messages'][0]['message'] == msg_send2
 
-    pass
 
+# User edits another user's message
 def test_message_edit_by_non_authorised_user():
-    
-    #Setup
-        #Register test user 1
-    token = message_test_helper.get_new_user1()[1]
 
-        #Create test channel 1
-    channel_id = channels.channels_create(token, 'test_channel1', True)['channel_id']
+    # Register test user 1 (owner)
+    email1, password1, name_first1, name_last1 = message_test_helper.get_new_user1(
+    )
+    register_retval1 = auth.auth_register(email1, password1, name_first1,
+                                          name_last1)
+    u_id1, token1 = register_retval1['u_id'], register_retval1['token']
 
-        #Send message 1
-    msg = 'The quick brown fox jumps over the lazy dog.'
-    message_id = message.message_send(token, channel_id, msg)
-    pass
+    # Register test user 2 (user)
+    email2, password2, name_first2, name_last2 = message_test_helper.get_new_user2(
+    )
+    register_retval2 = auth.auth_register(email2, password2, name_first2,
+                                          name_last2)
+    u_id2, token2 = register_retval2['u_id'], register_retval2['token']
 
-        #Log out test user 1
-    auth.auth_logout(token)
+    # Create test channel
+    ch_id = channels.channels_create(token1, 'test_channel1',
+                                     True)['channel_id']
+    channel.channel_join(token2, ch_id)  # user2 joins as member
 
-    #Register test user 2
-    token2 = message_test_helper.get_new_user2()[1]
+    # Send message 1
+    msg_send1 = 'The quick brown fox jumps over the lazy dog.'
+    msg_id = message.message_send(token1, ch_id, msg_send1)
 
-    #Actual test
-
-    newmsg = 'The quick brown dog jumps over the lazy fox.'
+    # Actual test
+    msg_send2 = 'The quick brown dog jumps over the lazy fox.'
     with pytest.raises(error.AccessError):
-        message.message_edit(token2, message_id, newmsg)    
-  
-    #Clean up (if necessary)
+        message.message_edit(token2, msg_id, msg_send2)
 
-    pass
 
+# Channel owner can edit any message
 def test_message_edit_by_owner():
 
-    #Setup
-        #Register test user 1
-    token = message_test_helper.get_new_user1()[1]
+    # Register test user 1 (owner)
+    email1, password1, name_first1, name_last1 = message_test_helper.get_new_user1(
+    )
+    register_retval1 = auth.auth_register(email1, password1, name_first1,
+                                          name_last1)
+    u_id1, token1 = register_retval1['u_id'], register_retval1['token']
 
-        #Create test channel 1
-    channel_id = channels.channels_create(token, 'test_channel1', True)['channel_id']
+    # Register test user 2
+    email2, password2, name_first2, name_last2 = message_test_helper.get_new_user2(
+    )
+    register_retval2 = auth.auth_register(email2, password2, name_first2,
+                                          name_last2)
+    u_id2, token2 = register_retval2['u_id'], register_retval2['token']
 
-        #Register test user 2
-    token2 = message_test_helper.get_new_user2()[1]
+    # Create test channel
+    ch_id = channels.channels_create(token1, 'test_channel1',
+                                     True)['channel_id']
+    channel.channel_join(token2, ch_id)  # user2 joins as member
 
-        #Test user 2 join test channel
-
-    channel.channel_join(token2, channel_id)
-
-        #Send message 1
-    msg = 'The quick brown fox jumps over the lazy dog.'
-    message_id = message.message_send(token2, channel_id, msg)
+    # Send message 1
+    msg_send1 = 'The quick brown fox jumps over the lazy dog.'
+    msg_id = message.message_send(token2, ch_id, msg_send1)
 
     #Actual test
-    newmsg = 'The quick brown dog jumps over the lazy fox.'
-    message.message_edit(token, message_id, newmsg)
+    msg_send2 = 'The quick brown dog jumps over the lazy fox.'
+    message.message_edit(token1, msg_id, msg_send2)
     start = 0
-    assert channel.channel_messages(token2, channel_id, start)['messages'][0]['message'] == newmsg    
-    
-    #Clean up (if necessary)
-    pass
+    assert channel.channel_messages(
+        token2, ch_id, start)['messages'][0]['message'] == msg_send2
 
-    
+
+# Message edit is empty therefore must be removed
+def test_message_edit_to_remove():
+
+    # Register test user 1 (owner)
+    email1, password1, name_first1, name_last1 = message_test_helper.get_new_user1(
+    )
+    register_retval1 = auth.auth_register(email1, password1, name_first1,
+                                          name_last1)
+    u_id1, token1 = register_retval1['u_id'], register_retval1['token']
+
+    # Create test channel
+    ch_id = channels.channels_create(token1, 'test_channel1',
+                                     True)['channel_id']
+
+    # Send message 1
+    msg_send1 = 'The quick brown fox jumps over the lazy dog.'
+    msg_id = message.message_send(token1, ch_id, msg_send1)
+
+    #Actual test
+    msg_send2 = ''
+    message.message_edit(token1, msg_id, msg_send2)
+    start = 0
+    channel_messages_retval = channel.channel_messages(token1, ch_id, start)
+    assert channel_messages_retval == {}
