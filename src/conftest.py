61f6b5a87c6359abe55d1817c9487f96de68ba82
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-08 20:28:52 +1100

    Added get_new_user_detail_a etc to conftest.py

--- src/conftest.py
+++ src/conftest.py
@@ -5,9 +5,33 @@ import auth
 
 # for auth.py generating a user who is going to register next
 @pytest.fixture(scope="session")
-def get_new_user():
+def get_new_user_detail_a():
     email = "z1234567@unsw.edu.au"
     password = "qwetyu"
+    name_first = "Dan"
+    name_last = "Nguyen"
+
+    return email, password, name_first, name_last
+
+def get_new_user_detail_b():
+    email = "z2345678@unsw.edu.au"
+    password = "qwetdd"
+    name_first = "Jack"
+    name_last = "Nassif"
+
+    return email, password, name_first, name_last
+
+def get_new_user_detail_c():
+    email = "z3456789@unsw.edu.au"
+    password = "qwetrb"
+    name_first = "Raymond"
+    name_last = "Soedargo"
+
+    return email, password, name_first, name_last
+
+def get_new_user_detail_d():
+    email = "z4567890@unsw.edu.au"
+    password = "qwetds"
     name_first = "Zhihan"
     name_last = "Qin"
 
@@ -20,6 +44,9 @@ def gen_person_info(get_new_user):
     email2 = 'z1234567@gmail.com'
     invalid_name_first = 'zaqwertyuioplmnbvcxsdfghjklpoiuytrewqazxsdcvfgbnhjmk'
     invalid_name_last = ''
+
+    invalid_name_first2 = ''
+    invalid_name_last2 = 'zaqwertyuioplmnbvcxsdfghjklpoiuytrewqazxsdcvfgbnhjmk'
 
     return email1, email2, password, name_first, name_last, invalid_name_first, invalid_name_last
 
