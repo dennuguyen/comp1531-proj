8dc6772db1fffbbfec2350825b8f5b36c0a991fb
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-15 13:40:26 +1100

    Add sudo codes for function implementations on channels.py
    -Add sudo codes for channel_invite
    -Add sudo codes for channel_details
    -Add sudo codes for channel_messages

--- src/channel.py
+++ src/channel.py
@@ -1,9 +1,43 @@
 def channel_invite(token, channel_id, u_id):
+
+# Check if token is valid
+    # Raise AccessError if token is not valid
+
+# Check if channel_id is valid
+    # Raise InputError if channel_id is not valid
+
+# Check if token is part of channel
+    # Raise InputError if token is not part of channel
+
+# Check if u_id is valid
+    # Raise InputError if u_id is not a valid u_id
+
+# Check if u_id is already a member
+    # Raise AccessError if u_id is already a member
+
+# If all conditions are met add u_id to channel as member
+    # Edit relevant databases
+
+# Return {}
+
     return {
     }
 
 
 def channel_details(token, channel_id):
+
+# Check that token is valid
+    # Raise AccessError if token is not valid
+
+# Check channel_id is valid
+    # Raise InputError if channel_id is not valid
+
+# Check token is member of channel
+    # Raise AccessError if token is not member of channel
+
+# If all conditions are met 
+    # Return {name, owner_members, all_members}
+
     return {
         'name': 'Hayden',
         'owner_members': [
@@ -24,6 +58,25 @@ def channel_details(token, channel_id):
 
 
 def channel_messages(token, channel_id, start):
+
+# Check token is valid
+    # Raise AccessError if token is not valid
+
+# Check channel_id is valid
+    # Raise InputError if channel_id is not valid
+
+# Check token is a member of channel
+    # Raise AccessError if token is not a member of channel
+
+# Check that start is equal to or greater than total number of messages in channel
+    # Raise InputError if start is euqal or greater than total number of messages
+
+# If all conditions are met
+    # If end is less than total messages
+        # Return {messages, start, end}
+    # Else if end is greater than total messages
+        # Return {messages, start, -1}
+
     return {
         'messages': [
             {
@@ -39,6 +92,8 @@ def channel_messages(token, channel_id, start):
 
 
 def channel_leave(token, channel_id):
+
+
     return {
     }
 
