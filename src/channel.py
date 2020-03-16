9ff102839184f90ac96d8e9925c7e20a4b7c8693
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-16 16:06:57 +1100

    Start writing codes for functions in channel.py

--- src/channel.py
+++ src/channel.py
@@ -4,12 +4,29 @@ def channel_invite(token, channel_id, u_id):
     # Raise AccessError if token is not valid
 
 # Check if channel_id is valid
+channels = channels_listall(token)
+ch_valid = False
+for x['channel_id'] in channels['channels']:
+    if channel_id = x['channel_id']:
+        ch_valid = True
     # Raise InputError if channel_id is not valid
+if ch_valid == False:
+    raise InputError('channel_id is not valid')
 
 # Check if token is part of channel
+channel_details = channel_details(token, channel_id)
+token_authorised = False
+for x['u_id'] in channel_details['all_members']:
+    if u_id = x['u_id']:
+        token_authorised = True
     # Raise InputError if token is not part of channel
-
+if token_authorised == False:
+    raise InputError('user is not member of channel')
+    
 # Check if u_id is valid
+
+
+
     # Raise InputError if u_id is not a valid u_id
 
 # Check if u_id is already a member
@@ -17,8 +34,6 @@ def channel_invite(token, channel_id, u_id):
 
 # If all conditions are met add u_id to channel as member
     # Edit relevant databases
-
-# Return {}
 
     return {
     }
@@ -93,21 +108,77 @@ def channel_messages(token, channel_id, start):
 
 def channel_leave(token, channel_id):
 
+# Check token is valid
+    # Raise AccessError if token is not valid
+
+# Check channel_id is valid
+    # Raise InputError if channel_id is not valid
+
+# Check token is a member of channel
+    # Raise AccessError if token is not a member of channel
+
+# If all conditions are met
+    # Remove user from channel member list
 
     return {
     }
 
 
 def channel_join(token, channel_id):
+
+# Check token is valid
+    # Raise AccessError if token is not valid
+
+# Check channel_id is valid
+    # Raise InputError if channel_id is not valid
+
+# Check channel is not private
+    # Raise AccessError if channel is private
+
+# If all conditons are met
+    # Add token to channel member list
+
     return {
     }
 
 
 def channel_addowner(token, channel_id, u_id):
+
+# Check token is valid
+    # Raise AccessError if token is not valid
+
+# Check token is authorised
+    # Raise AccessError if token is not authorised
+
+# Check channel_id is valid
+    # Raise InputError if channel_id is not valid
+
+# Check u_id is not already an owner
+    # Raise InputError if u_id is already an owner of the channel
+
+# If all conditons are met
+    # Add u_id into channel owner list
+
     return {
     }
 
 
 def channel_removeowner(token, channel_id, u_id):
+
+# Check token is valid
+    # Raise AccessError if token is not valid
+
+# Check token is authorised
+    # Raise AccessError if token is not authorised
+
+# Check channel_id is valid
+    # Raise InputError if channel_id is not valid
+
+# Check u_id is an owner
+    # Raise InputError if u_id is not an owner of the channel
+
+# If all conditons are met
+    # Remove u_id from channel owner list
+
     return {
     }
