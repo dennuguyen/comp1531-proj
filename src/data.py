46367cd07175caf05b70a601bee985f43781be6b
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-18 00:30:36 +1100

    Edit data structure

--- src/data.py
+++ src/data.py
@@ -1,9 +1,10 @@
-user = {'u_id' : 0, 'email' : 0, 'name_first' : 0, 'name_last' : 0, 'handle_str' : 0}
-users = {'users': []} # list of different user
-message = {'message_id' : 0, 'u_id' : 0, 'message' : 0, 'time_created' : 0}
-messages = {'messages':[]} # list of different message
-channel = {'channel_id' : 0, 'name' : 0}
-channels = {'channels':[]} # list of different channel
-member = {'u_id' : 0, 'name_first' : 0, 'name_last' : 0}
-channel_info = {'name' : 0, 'owner_members' : [], 'name_last' : []}
-channels_info = {'channels_info':[]} # list of different channels info
+import json
+
+data = {
+'users' : [{'u_id' : 0, 'email' : 0, 'name_first' : 0, 'name_last' : 0, 'handle_str' : 0}], # list of different user
+'messages' : [{'message_id' : 0, 'u_id' : 0, 'message' : 0, 'time_created' : 0}], # list of different message
+'channels' : [{'channel_id' : 0, 'name' : 0}], # list of different channel
+'channels_info' : [{'name' : 0, 'isprivate' : 0, 'owner_members' : [{'u_id' : 0, 'name_first' : 0, 'name_last' : 0}], 'name_last' : [{'u_id' : 0, 'name_first' : 0, 'name_last' : 0}]}], # list of different channels info
+}
+with open("data.json", "w+") as f:
+    json.dump(data, f)
