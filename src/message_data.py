d9e5b56f1954e8800389ae420adf53d9e0fc9aca
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-19 22:05:37 +1100

    Complete add functions related to send

---
+++ src/message_data.py
@@ -0,0 +1,16 @@
+'''
+data related operations for message.py;
+such as create, add, delete, search
+
+'''
+
+import data
+
+def get_uid_with_token(token):
+    pass
+
+def create_message_object(u_id, message):
+    pass
+
+def add_message_to_channel(u_id, message_id):
+    pass  
