259c481406910106d20798fac4b6af6d05d0a99b
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-19 11:17:03 +1100

    Add more functions

--- src/stubs/message.py
+++ src/stubs/message.py
@@ -3,44 +3,34 @@
 def message_send(token, channel_id, message):
 
     return {
-        'message_id': 1,
-        'u_id': 1,
-        'message': 'Hello World'
-        'time_created': 12345678
-        'reacts': 
-        'is_pinned': True
+        'message_id': 1
     }
 
 def message_sendlater(token, channel_id, message, time_sent):
+    
     return {
-        'message_id': 1,
+        'message_id': 1
     }
+
+def message_react(token, message_id, react_id):
+    return 
+
+def message_unreact(token, message_id, react_id):
+    return
+
+def message_pin(token, message_id):
+    return 
+
+def message_unpin(token, message_id):
+    return 
 
 def message_remove(token, message_id):
-    return {
-    }
+    return 
 
 def message_edit(token, message_id, message):
-    return {
-    }
+    return
 
-def react(token, message_id, react_id):
-    return {
 
-    }
 
-def unreact(token, message_id, react_id):
-    return {
 
-    }
-
-def pin(token, message_id):
-    return {
-
-    }
-
-def unpin(token, message_id):
-    return {
-
-    }
 
