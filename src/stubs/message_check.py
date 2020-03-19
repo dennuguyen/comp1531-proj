89d89b8d22cd3514d8379d4a8146547331d671f7
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-20 09:43:43 +1100

    Add parameters to functions

--- src/stubs/message_check.py
+++ src/stubs/message_check.py
@@ -3,3 +3,17 @@ check the validity of data for functions in message.py
 
 '''
 
+def is_valid_message():
+    pass
+
+def is_valid_channel():
+    pass
+
+def is_user_in_channel():
+    pass
+
+def is_valid_time():
+    pass
+
+def is_valid_reaction():
+    pass
