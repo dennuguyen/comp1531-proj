1ce64cb86cc9c43d7e8bd4266c2845c0c06d8c26
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-21 23:21:30 +1100

    Add implementations to channel.py

--- src/channel.py
+++ src/channel.py
@@ -16,9 +16,6 @@ def channel_invite(token=str, channel_id=int, u_id=int):
     return {
     }
 
-
-
-
 # @check_token_isvalid
 # @check_channel_id_isvalid
 # @check_token_isauthorised
@@ -27,7 +24,9 @@ def channel_details(token=str, channel_id=int):
 # If all conditions are met 
     # Return {name, owner_members, all_members}
     datapy = data.Data()
-    return datapy.get_channel_dict(channel_id)
+    channel_dict = datapy.get_channel_dict(channel_id)
+    channel_details = {'name' : channel_dict['name'], }
+
 
 # @check_token_isvalid
 # @check_channel_id_isvalid
@@ -41,18 +40,20 @@ def channel_messages(token=str, channel_id=int, start=int):
     # Else if end is greater than total messages
         # Return {messages, start, -1}
 
-    return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
-        'start': 0,
-        'end': 50,
-    }
+    datapy = data.Data()
+    msg_id_list = datapy.get_all_message_ids_in_a_channel(channel_id)
+    no_messages = len(msg_id_list)
+    show = start + 50
+    end_view = start + 50
+    if no_messages < (start + 50):
+        show = no_messages
+        end_view = -1
+        messages = []
+    x = 1
+    for i in msg_id_list and x <= show:
+        messages.append(datapy.get_message_dict(i))
+    return {'messages' : messages, 'start' : start, 'end' : end_view}
+
 
 # @check_token_isvalid
 # @check_channel_id_isvalid
@@ -62,6 +63,14 @@ def channel_leave(token=str, channel_id=int):
 
 # If all conditions are met
     # Remove user from channel member list
+
+
+    datapy = data.Data()
+    u_id = datapy.get_u_id_with_token(token)
+    u_id_list = datapy.get_owner_u_ids_with_channel_id(channel_id)
+    if u_id in u_id_list:
+        datapy.remove_user_to_channel(u_id, channel_id, True)
+    datapy.remove_user_to_channel(u_id, channel_id, False)
 
     return {
     }
@@ -73,6 +82,10 @@ def channel_join(token=str, channel_id=int):
 
 # If all conditons are met
     # Add token to channel member list
+
+    datapy = data.Data()
+    u_id = datapy.get_u_id_with_token(token)
+    datapy.add_user_to_channel(u_id, channel_id, False)
 
     return {
     }
@@ -86,6 +99,11 @@ def channel_addowner(token=str, channel_id=int, u_id=int):
 # If all conditons are met
     # Add u_id into channel owner list
 
+    datapy = data.Data()
+    u_id = datapy.get_u_id_with_token(token)
+    datapy.remove_user_to_channel(u_id, channel_id, False)
+    datapy.add_user_to_channel(u_id, channel_id, True)
+
     return {
     }
 
@@ -98,6 +116,9 @@ def channel_removeowner(token=str, channel_id=int, u_id=int):
 
 # If all conditons are met
     # Remove u_id from channel owner list
+
+    datapy = data.Data()
+    datapy.remove_user_to_channel(u_id, channel_id, True)
 
     return {
     }
