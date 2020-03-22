beb38b90edff72a7b58b5fcf495eac212ea96f5a
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-23 01:40:11 +1100

    Add more functions for react and unreact

--- src/message_check.py
+++ src/message_check.py
@@ -1,6 +1,7 @@
 '''
 check the validity of data for functions in message.py
 '''
+from data import getData
 
 def is_valid_message(message):
     return True
@@ -18,4 +19,13 @@ def is_valid_reaction(reaction):
     return True
 
 def is_message_in_channel(message_id, channel_id):
+    return True
+
+def is_there_message(message_id):
+    return True
+
+def is_valid_react_id(react_id):
+    return True if react_id == 1 else False
+
+def is_not_reacted_yet(message_id, react_id):
     return True
