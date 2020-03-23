17ad56e210d555ca213cfbfa29c7fe0f1def7865
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-24 00:19:27 +1100

    Delete the priority queue

--- src/message_util.py
+++ src/message_util.py
@@ -3,16 +3,9 @@ util file for message.py
 '''
 
 import time
-from queue import PriorityQueue
 
 CURRENT_TIME = int(time.time())
 
 def get_current_time():
     global CURRENT_TIME
     return CURRENT_TIME
-
-MESSAGE_QUEUE = PriorityQueue()
-
-def get_message_queue():
-    global MESSAGE_QUEUE
-    return MESSAGE_QUEUE
