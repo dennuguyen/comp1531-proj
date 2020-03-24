b5936dbe5c8aac63de8c5a24c78908ae4072819d
dan <dennuguyen@gmail.com>
2020-03-25 05:20:57 +1100

    Give example of implementation of functions using data.py in channel.py and channels.py

--- src/channel.py
+++ src/channel.py
@@ -11,6 +11,7 @@ from channel_decorator import check_channel_isnotprivate
 from channel_decorator import check_token_isowner
 from channel_decorator import check_u_id_isnotowner
 from channel_decorator import check_u_id_isowner
+import data
 
 @check_token_isvalid
 @check_channel_id_isvalid
@@ -33,22 +34,22 @@ def channel_details(token=str, channel_id=int):
 # If all conditions are met 
     # Return {name, owner_members, all_members}
 
+def channel_details(token, channel_id):
+    owner_members_list = []
+    all_members_list = []
+
+    for owner_u_id in data.get_data().get_channel_with_ch_id(channel_id).get_owner_u_id_list():
+        # Get the owner's information from their u_id
+        owner_members_list.append(data.get_data().get_user_with_u_id(owner_u_id).get_member_details_dict())
+    
+    for u_id in data.get_data().get_channel_with_ch_id(channel_id).get_u_id_list():
+        # Get the user's information from their u_id
+        all_members_list.append(data.get_data().get_user_with_u_id(u_id).get_member_details_dict())
+
     return {
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-            }
-        ],
+        'name' : data.get_data().get_channel_with_ch_id(channel_id).get_channel_name(),
+        'owner_members' : owner_members_list,
+        'all_members' : all_members_list,
     }
 
 @check_token_isvalid
