92c5629b0d4b73bd6243dfdb1b7a2ac2edee7807
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-26 03:21:29 +1100

    Finish implementing the functions

--- src/standup.py
+++ src/standup.py
@@ -1,31 +1,52 @@
 '''
 Standup related functions are here
 '''
-
-from data import get_data
+import time
+import threading
+from data import get_data, Message
 import authenticate as au
 
 @au.authenticator(au.is_token_valid, au.valid_channel_id, au.already_active_standup)
 def standup_start(token, channel_id, length):
     ''' Add a message to the message queue at the end of the message queue in channel.'''
-    u_id = get_data().get_user_with_token(token).get_u_id()
 
+     # start a standup
+    channel = get_data().get_channel_with_ch_id(channel_id)
+    channel.set_standup_status(True)
+    channel.set_standup_time_finish(int(time.time())+length)
+    threading.Timer(length, channel.pop_standup_queue_into_message_queue())
+    
+    # reset the data after finishing the standup
+    channel.set_standup_status(False)
+    channel.set_standup_time_finish(-1)
 
     return {
-        'time_finish' : 0
+        'time_finish' : channel.get_standup_time_finish()
     }
 
 @au.authenticator(au.is_token_valid, au.valid_channel_id)
 def standup_active(token, channel_id):
     ''' For a given channel, return whether a standup is active in it, and what time the standup finishes. '''
-    
+
+    channel = get_data().get_channel_with_ch_id(channel_id)
     return {
-        'is_active' : False,
-        'time_finish' : 0
+        'is_active' : channel.get_is_active_standup,
+        'time_finish' : channel.get_standup_time_finish()
     }
 
 @au.authenticator(au.is_token_valid, au.valid_channel_id, au.is_user_in_channel, au.message_length, au.already_active_standup)
 def standup_send(token, channel_id, message):
     ''' Sending a message to get buffered in the standup queue, assuming a standup is currently active.'''
-    
+
+    # setup the message
+    u_id = get_data().get_user_with_token(token).get_u_id()
+    message_id = get_data().global_msg_id()
+    time_created = int(time.time())
+    message_object = Message(message_id, u_id, message, time_created)
+
+     # update the database
+    get_data().add_message(message_object)
+    channel = get_data().get_channel_with_ch_id(channel_id)
+    channel.add_message_to_standup_queue(message_id)
+
     return {}
