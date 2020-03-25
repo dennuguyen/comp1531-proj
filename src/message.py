77b87c8f32c7ca5b052a407f3910880b97e7cbf2
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-26 01:44:24 +1100

    Complete implementing the functions

--- src/message.py
+++ src/message.py
@@ -2,194 +2,132 @@
 Message related operations are here.
 '''
 
-import message_check
-from data import getData, Message
-from message_util import get_current_time
-import error
+import time
+import authenticate as au
+from data import get_data, Message
 
-
+@au.authenticator(au.is_token_valid, au.is_user_in_channel, au.valid_channel_id, au.message_length)
 def message_send(token, channel_id, message):
     '''
     Send a message from authorised_user to the channel specified
     by channel_id
     '''
-    u_id = getData().get_u_id_with_token(token)
-
-    # check validity of the input augments
-    if not message_check.is_user_in_channel(u_id, channel_id):
-        raise error.AccessError(description="The user is not in this channel.")
-    if not message_check.is_valid_channel(channel_id):
-        raise error.InputError(description="The channel does not exist.")
-    if not message_check.is_valid_message(message):
-        raise error.InputError(description="The message is more than 1000 characters.")
+    u_id = get_data().get_user_with_token(token).get_u_id()
 
     # setup the message
-    message_id = getData().gen_next_channel_id()
-    time_created = get_current_time()
+    message_id = get_data().global_msg_id()
+    time_created = int(time.time())
     message_object = Message(message_id, u_id, message, time_created)
 
     # update the database
-    getData().add_message(message_object)
-    getData().add_message_to_channel(message_id, channel_id)
+    get_data().add_message(message_object)
+    channel = get_data().get_channel_with_ch_id(channel_id)
+    channel.add_new_message(message_id)
 
     return {
         'message_id': message_id
     }
 
+@au.authenticator(au.is_token_valid, au.is_user_in_channel, au.valid_channel_id,
+                    au.message_length, au.send_message_in_future)
 def message_sendlater(token, channel_id, message, time_sent):
     '''
     Send a message from authorised_user to the channel specified
     by channel_id automatically at a specified time in the future
     '''
-    u_id = getData().get_u_id_with_token(token)
-
-    # check validity of the input augments
-    if not message_check.is_user_in_channel(u_id, channel_id):
-        raise error.AccessError(description="The user is not in this channel.")
-    if not message_check.is_valid_channel(channel_id):
-        raise error.InputError(description="The channel does not exist.")
-    if not message_check.is_valid_message(message):
-        raise error.InputError(description="The message is more than 1000 characters.")
-    if not message_check.is_valid_time(time_sent):
-        raise error.InputError(description="Time sent is a time in the past.")
+    u_id = get_data().get_user_with_token(token).get_u_id()
 
     # setup the message
-    message_id = getData().gen_next_channel_id()
-    time_created = get_current_time()
+    message_id = get_data().global_msg_id()
+    time_created = int(time.time())
     message_object = Message(message_id, u_id, message, time_created)
 
     # update the database
-    getData().add_message_to_waitlist(message_object)
+    get_data().add_message_later(message_object)
 
     return {
         'message_id': message_id
     }
 
+@au.authenticator(au.message_id_valid, au.is_token_valid, au.valid_channel_id,
+                  au.is_user_in_channel, au.is_message_id_in_channel,
+                  au.is_valid_react_id, au.already_contains_react)
 def message_react(token, message_id, react_id):
     '''
     Given a message within a channel the authorised user is part of,
     add a "react" to that particular message
     '''
-    # check validity of the input augments
-    if not message_check.is_there_message(message_id):
-        raise error.InputError(description="The message does not exsit.")
-
-    u_id = getData().get_u_id_with_token(token)
-    channel_id = getData().get_channel_id(message_id)
-    if not message_check.is_user_in_channel(u_id, channel_id):
-        raise error.AccessError(description="The user is not in this channel.")
-    if not message_check.is_message_in_channel(message_id, channel_id):
-        raise error.InputError(description="The message is not in this channel.")
-
-    if not message_check.is_valid_react_id(react_id):
-        raise error.InputError(description="This is an invalid reaction.")
-    if message_check.is_reacted(message_id, react_id):
-        raise error.InputError(description="The message has already been reacted with this reaction.")
 
     # update the database
-    message_object = getData().get_message(message_id)
+    u_id = get_data().get_user_with_token(token).get_u_id()
+    message_object = get_data().get_message_with_message_id(message_id)
     message_object.set_react(react_id, u_id, True)
 
     return {}
 
+@au.authenticator(au.message_id_valid, au.is_token_valid, au.valid_channel_id,
+                  au.is_user_in_channel, au.is_message_id_in_channel,
+                  au.is_valid_react_id, au.does_not_contain_react)
 def message_unreact(token, message_id, react_id):
     '''
     Given a message within a channel the authorised user is part of,
     remove a "react" to that particular message
     '''
-    # check validity of the input augments
-    if not message_check.is_there_message(message_id):
-        raise error.InputError(description="The message does not exsit.")
-
-    u_id = getData().get_u_id_with_token(token)
-    channel_id = getData().get_channel_id(message_id)
-    if not message_check.is_user_in_channel(u_id, channel_id):
-        raise error.AccessError(description="The user is not in this channel.")
-    if not message_check.is_message_in_channel(message_id, channel_id):
-        raise error.InputError(description="The message is not in this channel.")
-
-    if not message_check.is_valid_react_id(react_id):
-        raise error.InputError(description="This is an invalid reaction.")
-    if not message_check.is_reacted(message_id, react_id):
-        raise error.InputError(description="The message has not been reacted with this reaction yet.")
 
     # update the database
-    message_object = getData().get_message(message_id)
+    u_id = get_data().get_user_with_token(token).get_u_id()
+    message_object = get_data().get_message_with_message_id(message_id)
     message_object.set_react(react_id, u_id, False)
 
     return {}
 
+@au.authenticator(au.message_id_valid, au.is_token_valid, au.valid_channel_id,
+                  au.is_user_in_channel, au.is_message_id_in_channel,
+                  au.not_ch_owner_or_owner, au.message_already_pinned)
 def message_pin(token, message_id):
     '''
     Given a message within a channel, mark it as "pinned" to be given
     special display treatment by the frontend
     '''
-    # check validity of the input augments
-    if not message_check.is_there_message(message_id):
-        raise error.InputError(description="The message does not exsit.")
-
-    u_id = getData().get_u_id_with_token(token)
-    channel_id = getData().get_channel_id(message_id)
-    if not message_check.is_user_in_channel(u_id, channel_id):
-        raise error.AccessError(description="The user is not in this channel.")
-    if not message_check.is_message_in_channel(message_id, channel_id):
-        raise error.InputError(description="The message is not in this channel.")
-    if not message_check.is_user_the_owner(u_id, channel_id):
-        raise error.InputError(description="The user is not an owner of this channel.")
-    if message_check.is_pinned(message_id):
-        raise error.InputError(description="The message is already pinned.")
 
     # update the database
-    message_object = getData().get_message(message_id)
+    message_object = get_data().get_message_with_message_id(message_id)
     message_object.set_is_pinned(True)
 
     return {}
 
+@au.authenticator(au.message_id_valid, au.is_token_valid, au.valid_channel_id,
+                  au.is_user_in_channel, au.is_message_id_in_channel,
+                  au.not_ch_owner_or_owner, au.message_already_unpinned)
 def message_unpin(token, message_id):
     '''
     Given a message within a channel, remove it's mark as unpinned
     '''
-    # check validity of the input augments
-    if not message_check.is_there_message(message_id):
-        raise error.InputError(description="The message does not exsit.")
-
-    u_id = getData().get_u_id_with_token(token)
-    channel_id = getData().get_channel_id(message_id)
-    if not message_check.is_user_in_channel(u_id, channel_id):
-        raise error.AccessError(description="The user is not in this channel.")
-    if not message_check.is_message_in_channel(message_id, channel_id):
-        raise error.InputError(description="The message is not in this channel.")
-    if not message_check.is_user_the_owner(u_id, channel_id):
-        raise error.InputError(description="The user is not an owner of this channel.")
-    if not message_check.is_pinned(message_id):
-        raise error.InputError(description="The message is already pinned.")
 
     # update the database
-    message_object = getData().get_message(message_id)
+    message_object = get_data().get_message_with_message_id(message_id)
     message_object.set_is_pinned(False)
+
     return {}
 
+@au.authenticator(au.message_id_valid, au.is_token_valid, au.edit_permissions)
 def message_remove(token, message_id):
     '''
     Given a message_id for a message,
     this message is removed from the channel
     '''
 
-    # check validity of the input augments
-    if not message_check.is_there_message(message_id):
-        raise error.InputError(description="The message does not exsit.")
-
-    u_id = getData().get_u_id_with_token(token)
-    channel_id = getData().get_channel_id(message_id)
-    if not message_check.is_message_sent_by_user(message_id, u_id) and \
-        not message_check.is_user_the_owner(u_id, channel_id):
-        raise error.AccessError(description="The message is not sent by this user.")
-
     # update the database
-    getData().remove_message_from_channel(message_id, channel_id)
+    message_object = get_data().get_message_with_message_id(message_id)
+    get_data().remove_message(message_object)
+    for channel in get_data().get_channel_list():
+        for msg_id in channel.get_msg_id_list():
+            if msg_id == message_id:
+                channel.remove_message(message_id)
 
     return {}
 
+@au.authenticator(au.message_id_valid, au.is_token_valid, au.edit_permissions)
 def message_edit(token, message_id, message):
     '''
     Given a message, update it's text with new text.
@@ -199,17 +137,8 @@ def message_edit(token, message_id, message):
     if not message:
         message_remove(token, message_id)
 
-    # check validity of the input augments
-    if not message_check.is_there_message(message_id):
-        raise error.InputError(description="The message does not exsit.")
-
-    u_id = getData().get_u_id_with_token(token)
-    channel_id = getData().get_channel_id(message_id)
-    if not message_check.is_message_sent_by_user(message_id, u_id) and \
-        not message_check.is_user_the_owner(u_id, channel_id):
-        raise error.AccessError(description="The message is not sent by this user.")
-    
     # update the database
-    message_object = getData().get_message(message_id)
+    message_object = get_data().get_message_with_message_id(message_id)
     message_object.set_message(message)
+
     return {}
