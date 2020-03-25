f3d97a3292b31a14f7e7f9782a8e60dda655f9b8
Giacomo Nassif <jacknassif9999@gmail.com>
2020-03-25 18:44:27 +1100

    Implemented user.py. Still TODO: implement authenticator.py with this file

--- src/user.py
+++ src/user.py
@@ -1,41 +1,64 @@
-import decorators
+'''
+user.py has four main functions.
 
+user_profile(.) - returns a user dictionary given a u_id
 
-@decorators.authenticate_token
-@decorators.authenticate_u_id
+user_profile_setname(.) - allows updating of first and last names
+
+user_profile_setemail(.) - allows updating of email.
+
+user_profile_sethandle(.) - allows updating of handles.
+'''
+
+import data
+
+# TODO: Decide on the structure of authenticate.py import.
+
 def user_profile(*, token, u_id):
-
-    print('Hello\n')
-
-    return {
-        'user': {
-            'u_id': u_id,
-            'email': email,
-            'name_first': name_first,
-            'name_last': name_last,
-            'handle_str': handle_str,
-        },
-    }
+    '''
+    For a valid user, returns information about their user id, email,
+    first name, last name, and handle.
+    '''
+    # Get the User dataclass with u_id.
+    user = data.get_data().get_user_with_u_id(u_id)
+    return {'user' : user.get_user_dict()}
 
 
-@decorators.authenticate_token
-@decorators.authenticate_name_first
-@decorators.authenticate_name_last
-def user_profile_setname(token, name_first, name_last):
+def user_profile_setname(*, token, name_first, name_last):
+    '''
+    Update the authorised user's first and last name.
+    '''
+    # Get user class with token
+    user = data.get_data().get_user_with_token(token)
+
+    # Update the corresponding name
+    user.set_name_first(name_first)
+    user.set_name_last(name_last)
 
     return {}
 
 
-@decorators.authenticate_token
-@decorators.authenticate_email
-def user_profile_setemail(token, email):
+def user_profile_setemail(*, token, email):
+    '''
+    Update the authorised user's email address.
+    '''
+    # Get user class with token
+    user = data.get_data().get_user_with_token(token)
+
+    # Update the corresponding email
+    user.set_email(email)
+
     return {}
 
 
-@decorators.authenticate_token
-@decorators.authenticate_handle_str
-def user_profile_sethandle(token, handle_str):
+def user_profile_sethandle(*, token, handle_str):
+    '''
+    Update the authorised user's handle (i.e. display name).
+    '''
+    # Get user class with token
+    user = data.get_data().get_user_with_token(token)
+
+    # Update the corresponding handle_str
+    user.set_handle_str(handle_str)
+
     return {}
-
-
-user_profile(token='token', u_id=3)
