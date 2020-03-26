653278b1a42a69e014f539614d6b2d0778466d21
Sunny Qing <z5290141@student.unsw.edu.au>
2020-03-26 23:35:42 +1100

    Add reset functions at the each of test function

--- src/test/message_send_test.py
+++ src/test/message_send_test.py
@@ -3,30 +3,29 @@ import message
 import channel
 import channels
 import error
-import datetime
+import time
 import other
+from data import get_data
 import sys
 sys.path.append('../')
-
 
 # User sends a message to a channel they are a member of
 def test_message_send_member(get_new_user_1):
 
     # Register test user 1 (owner)
     u_id1, token1 = get_new_user_1
-
+    
     # Create test channel
-    ch_id = channels.channels_create(token1, 'test_channel1',
-                                     True)['channel_id']
-
+    ch_id = channels.channels_create(token=token1, name='test_channel1',
+                                     is_public=True)['channel_id']
     # Send message 1
     msg_send1 = 'The quick brown fox jumps over the lazy dog'
-    time_before1 = datetime.datetime.now()
-    msg_id1 = message.message_send(token1, ch_id, msg_send1)['message_id']
-    time_after1 = datetime.datetime.now()
+    time_before1 = int(time.time())
+    msg_id1 = message.message_send(token=token1, channel_id=ch_id, message=msg_send1)['message_id']
+    time_after1 = int(time.time())
 
     # Check message 1
-    retval1 = other.search(token1, msg_send1)['messages']
+    retval1 = other.search(token=token1, query_str=msg_send1)['messages']
     assert retval1[0]['message_id'] == msg_id1
     assert retval1[0]['u_id'] == u_id1
     assert retval1[0]['message'] == msg_send1
@@ -36,12 +35,12 @@ def test_message_send_member(get_new_user_1):
 
     # Send message 2
     msg_send2 = 'The quick brown dog jumps over the lazy fox'
-    time_before2 = datetime.datetime.now()
-    msg_id2 = message.message_send(token1, ch_id, msg_send2)['message_id']
-    time_after2 = datetime.datetime.now()
+    time_before2 = int(time.time())
+    msg_id2 = message.message_send(token=token1, channel_id=ch_id, message=msg_send2)['message_id']
+    time_after2 = int(time.time())
 
     # Check message 2
-    retval2 = other.search(token1, msg_send2)['messages']
+    retval2 = other.search(token=token1, query_str=msg_send2)['messages']
     assert retval2[0]['message_id'] == msg_id2
     assert retval2[0]['u_id'] == u_id1
     assert retval2[0]['message'] == msg_send2
@@ -50,14 +49,16 @@ def test_message_send_member(get_new_user_1):
     assert len(retval2) == 1
 
     # Check both messages are in channel
-    retval3 = channel.channel_messages(token1, ch_id, 0)['messages']
+    retval3 = channel.channel_messages(token=token1, channel_id=ch_id, start=0)['messages']
     assert len(retval3) == 2
 
+    get_data().reset()
 
 # Stranger to channel sends a message to that channel
 def test_message_send_stranger(get_new_user_1, get_new_user_2):
 
     # Register test user 1 (owner)
+    print(get_data().get_user_with_email('hugh_jackman@unsw.com'))
     _, token1 = get_new_user_1
 
     # Register test user 2 (stranger)
@@ -72,10 +73,12 @@ def test_message_send_stranger(get_new_user_1, get_new_user_2):
     with pytest.raises(error.AccessError):
         message.message_send(token2, ch_id, msg_send)
 
+    get_data().reset()
 
 # Message is more than 1000 char
 def test_message_send_1000_char(get_new_user_1):
 
+    get_data().reset()
     # Register test user 1 (owner)
     _, token1 = get_new_user_1
 
@@ -88,3 +91,5 @@ def test_message_send_1000_char(get_new_user_1):
     message.message_send(token1, ch_id, msg_send)
     with pytest.raises(error.InputError):
         message.message_send(token1, ch_id, msg_send)
+
+    get_data().reset()
