9460de9090513b42f50d655ffe3e25c43598d086
Giacomo Nassif <jacknassif9999@gmail.com>
2020-03-26 20:17:09 +1100

    Fixed

--- src/test/user_profile_setemail_test.py
+++ src/test/user_profile_setemail_test.py
@@ -2,6 +2,7 @@ import pytest
 import user
 import error
 import sys
+import data
 sys.path.append('../')
 
 # user changes their own email
@@ -9,14 +10,17 @@ sys.path.append('../')
 
 def test_user_profile_setemail(get_new_user_1, get_new_user_detail_1):
 
+
+    print("\n\nLMAO TEST 1 #########\n\n")
+
     # Register test user 1
     u_id, token = get_new_user_1
     email, _, name_first, name_last = get_new_user_detail_1
 
     # Actual test
     new_email = 'prefix' + email
-    user.user_profile_setemail(token, new_email)
-    assert user.user_profile(token, u_id) == {
+    user.user_profile_setemail(token=token,email=new_email)
+    assert user.user_profile(token=token,u_id=u_id) == {
         'user': {
             'u_id': u_id,
             'email': new_email,
@@ -25,20 +29,26 @@ def test_user_profile_setemail(get_new_user_1, get_new_user_detail_1):
             'handle_str': (name_first + name_last).lower(),
         },
     }
+    # Clean the data
+    data.get_data().reset()
 
 
 # email address of incorrect format
 def test_user_profile_setemail_invalid_email(get_new_user_1):
 
+    print("\n\nLMAO TEST 2 #########\n\n")
     # Register test user 1
     _, token = get_new_user_1
 
     # Actual test
     with pytest.raises(error.InputError):
-        user.user_profile_setemail(token, 'z1111111x.unsw.edu.au')
+        user.user_profile_setemail(token=token,email='z1111111x.unsw.edu.au')
 
     with pytest.raises(error.InputError):
-        user.user_profile_setemail(token, 'z1111111xunsweduau')
+        user.user_profile_setemail(token=token,email='z1111111xunsweduau')
+
+    # Clean the data
+    data.get_data().reset()
 
 
 # email address already used
@@ -46,11 +56,16 @@ def test_user_profile_setemail_email_already_used(get_new_user_1,
                                                   get_new_user_detail_1,
                                                   get_new_user_2):
 
+    print("\n\nLMAO TEST 3 #########\n\n")
     # Register test user 1 and 2
     _, _ = get_new_user_1
     email, _, _, _ = get_new_user_detail_1
     _, token2 = get_new_user_2
 
+
     # Actual test
     with pytest.raises(error.InputError):
-        user.user_profile_setemail(token2, email)
+        user.user_profile_setemail(token=token2,email=email)
+
+    # Clean the data
+    data.get_data().reset()
