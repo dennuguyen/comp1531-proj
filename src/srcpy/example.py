1b7f20a37b926aa2cb8d3b20cc9509c015ed042b
Raymond Soedargo <z3063670@student.unsw.edu.au>
2020-03-29 19:26:21 +1100

    Delete uncessesary file

--- src/srcpy/example.py
+++
@@ -1,6 +0,0 @@
-arr = {'in': [{'token': 0}, {'token': 1}]}
-
-try:
-    arr['in'].remove(token)
-except:
-    pass
--- src/srcpy/stubs/message_data.py
+++
@@ -1,6 +0,0 @@
-'''
-data related operations for message.py;
-such as create, input and output
-
-'''
-
